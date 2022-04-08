d5162eab951e33a04d595f2667268c251c1b2364
lautaro disbroiavacca <111992@tecnicatura.frc.utn.edu.ar>
2022-04-08 13:07:34 -0300

    Migracion de la BD

---
+++ ApiPracticaFinal/ApiPracticaFinal/Models/Presupuesto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace ApiPracticaFinal.Models
+{
+    public partial class Presupuesto
+    {
+        public int Idpresupuesto { get; set; }
+        public double Honorario { get; set; }
+        public double Viatico { get; set; }
+        public double Equipamiento { get; set; }
+        public double Gastos { get; set; }
+        public int Idproyecto { get; set; }
+        public char? Trial477 { get; set; }
+
+        public virtual Proyecto IdproyectoNavigation { get; set; }
+    }
+}
